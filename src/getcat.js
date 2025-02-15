9348ffba5cb4252c76c9a1134373105dc5770047
Kjell O E <kjell.omdal.erichsen@testify.no>
2025-02-15 13:10:05 +0100

    Added support for command line options and a few fixes

--- src/getcat.js
+++ src/getcat.js
@@ -1,15 +1,18 @@
+#!/usr/bin/env node
 const process = require('process')
 const fs = require('fs')
 const path = require('path')
 const crypto = require('crypto')
-
+const readline = require('node:readline');
 // Set up configuiration handling first load file, then override with environment variables
+const VERSION = '0.9.2' // getCAT version
 const CONFIG = {} // In memory config object
 const CONFIG_FIELDS = { ROOTCAPATH: null, NODETLSIGNORE: false, AUTHSYSTEMNAME: null, AUTHUSERNAME: null, AUTHTOKENFILE: null, AUTHTOKENSTRING: null, REQUESTAUTHURL: null, REQUESTTOKENURL: null} // Allowed config fields
 
 // Define class variables
 let tokenForSystem = null
 let tokenForIdent = null
+let tokenBase64 = null
 
 // Update the derivied values from config
 function updateConfigDerivedValues() {
@@ -61,7 +64,7 @@ function removeDefaultHeader(key) {
 
 // NOTE: Disabling TLS check for all certificates to allow for self signed ones used in test.
 function checkForCertificateError(errorMessage) {
-  if (errorMessage && errorMessage.includes('self-signed certificate in certificate chain') || errorMessage.includes('SELF_SIGNED_CERT_IN_CHAIN')) {
+  if (errorMessage && errorMessage.toString().includes('self-signed certificate in certificate chain') || errorMessage.toString().includes('SELF_SIGNED_CERT_IN_CHAIN')) {
     if (fs.existsSync(CONFIG.ROOTCAPATH)) {
       console.warn(`WARNING: Cannot run node requests due to TLS issue. Before running command again please set env: export NODE_EXTRA_CA_CERTS='${rootCertCaPath}'`)
     }
@@ -69,6 +72,89 @@ function checkForCertificateError(errorMessage) {
       console.warn('WARNING: Tests cannot run due to TLS issue. Please make sure env "NODE_EXTRA_CA_CERTS" points to a valid root CA')
     }
     console.warn('\tAlternatively disable TLS checking by setting env NODE_TLS_REJECT_UNAUTHORIZED to 0 or getcat CONFIG.NODETLSIGNORE to true')
+  }
+}
+
+// Mimic bash style command line help
+function processCommandlineArguments(processArguments) {
+  // Process getcat command line arguments // First argument is node, second is script. Third onwards are positional arguments from command line
+  if (processArguments[1] && processArguments[1].toLowerCase().endsWith('getcat.js')){
+    const args = processArguments.slice(2)
+    const firstArgument = (args[0]? args[0] : '--help') // Default
+    console.log(`getCAT-${VERSION} command line input: ${firstArgument.substring(('--').length)}`)
+    switch(firstArgument)  {
+      case '--help':
+        console.log('Quick usage: import getcat directly into your nodejs scripts to do getcat.requests.GET,POST,PUT and DELETE in quick and easy fashion')
+        console.log('Valid command line options: \n--help\n--usage\n--config\n\--configsave\n--install\n--authorize')
+        break;
+      case '--usage':
+      case '--examples':
+        console.log('Examples of usage: \nconst getcat = require("./getcat.js")\nconst jsonResponseData = await getcat.requests.GET("my-url.com/mypath")')
+        break;
+      case '--install':
+        console.log('No installation needed, just copy/clone/download from https://github.com/kjelloe/getcat/blob/main/src/getcat.js and go!')
+        break;
+      case '--config':
+        console.log('Config will be read getcat.config first, then the same fields will be read from ENV i.e "export NODETLSIGNORE=true" will override value in config file')
+        console.log('Current config:\n',CONFIG)
+        const envFieldsFound = Object.keys(CONFIG_FIELDS).filter( key => process.env[key])
+        console.log('The following ENVIRONMENT variables were found and their values will be applied after current config:\n', envFieldsFound.join('\n '))
+        if (envFieldsFound.length==0) { console.log(' -- NONE -- ') }
+        break;
+      case '--save':
+      case '--saveconfig':
+      case '--configsave':
+        const savefilepath = processArguments[1].replace('.js', '.config')
+        console.log('Current config:\n', CONFIG)
+        console.log('Will be saved to file: '+ savefilepath)
+        getcat.file.writeJsonToFile(CONFIG, savefilepath)
+        break;
+      case '--auth':
+      case '--authorize':
+          const readUser = (args[1]? args[1].trim() : process.env.USER) // Get from command line or use linux en variable
+          console.log(`getCAT will try to authorize and obtain key using "CONFIG.REQUESTTOKENURL" :`, CONFIG.REQUESTAUTHURL)
+          if (!CONFIG.REQUESTAUTHURL) throw new Error(`Cannot authorize, missing "CONFIG.REQUESTTOKENURL"`)
+          // Create local handler to read input of password
+          const readlineHandler = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+          })
+
+          readlineHandler.question(`getCAT will send request using base64 encoded username \"${readUser}\" and password provided. Please enter now:`, readPass => {
+            console.log(`\nSending authorization request...`)
+            readlineHandler.close() // Input ended
+            readlineHandler.history = readlineHandler.history.slice(1) // Clear history
+            // NOTE: All special chars are encoded https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
+            const authHashBase64 = getcat.misc.base64Encode(unescape(encodeURIComponent(readUser)) + ':' + unescape(encodeURIComponent(readPass)))
+            // DEBUG: console.log(authHashBase64)
+            getcat.auth.getAuthorizeToken(authHashBase64).then( authorizeToken => {
+                if (authorizeToken===false) throw new Error(`Authorization failed. Please check url, username and password`)
+                if (CONFIG.AUTHTOKENSTRING) {
+                  console.log(`Retrieved authorization token. Setting it to ENV variable \"AUTHTOKENSTRING\" allowing getCAT to use it for subsequent requests"`)
+                  process.env['AUTHTOKENSTRING'] = authorizeToken
+                }
+                else if (CONFIG.AUTHTOKENFILE) {
+                  console.log(`Retrieved authorization token. Writing it to file stated in ENV variable \"AUTHTOKENFILE\" allowing getCAT to use it for subsequent requests"`)
+                  getcat.file.writeStringToFile(authorizeToken, CONFIG.AUTHTOKENFILE, true)
+                }
+                else {
+                  console.log(`Retrieved authorization token. No \"AUTHTOKENFILE\" nor \"AUTHTOKENSTRING\" configured, writing key on next line:`)
+                  console.log(authorizeToken)
+                }
+                console.log('Authorization request completed. ')
+            })
+          })
+
+          // Override function to hide password entered
+          readlineHandler._writeToOutput = function _writeToOutput(stringToWrite) { readlineHandler.output.write("*") } 
+
+
+          // Write to file? getcat.file.writeJsonToFile(CONFIG, savefilepath)
+          // Or use env ? AUTHTOKENSTRING
+          break;
+      default:
+        console.log(`Unknown option "${firstArgument}". Try --help`)
+    }
   }
 }
 // If needing a a handler for any uncaught exceptions:
@@ -121,22 +207,29 @@ const getcat = {
     },
     _inner : {
       requestPostAndWaitForResponse: async function(method, systemUrl, postJsonObject, klientId, acceptType, contentType) {
-        const envSTokenPath = CONFIG.AUTHTOKENFILE
-        if (envSTokenPath==null) throw new Error(`CONFIG.AUTHTOKENFILE ${(envSTokenPath==null? 'has not been set. ' : `"${envSTokenPath}" does not exist.`)}`)
-        if (!getcat.auth.isValidAccessToken(envSTokenPath, 24)) throw new Error(`CONFIG.AUTHTOKENFILE hentet fra "${envSTokenPath}" har gått ut og må fornyes.`)
+        const envSTokenPath = (CONFIG.AUTHTOKENSTRING? CONFIG.AUTHTOKENSTRING : CONFIG.AUTHTOKENFILE)  // if tokenstring is enabled, use it instead of file
+        if (tokenBase64!==null && tokenBase64.length>0) {   // If basic auth is used, apply base64 string
+          return getcat.requests._inner.requestPostAndWaitForResponseWithToken(method, tokenBase64, systemUrl, postJsonObject, klientId, acceptType, contentType)
+        }
+        if (envSTokenPath===false) { // If tokenfile is disabled, skip and do request
+          return getcat.requests._inner.requestPostAndWaitForResponseWithToken(method, false, systemUrl, postJsonObject, klientId, acceptType, contentType)
+        }
+        // Proceed to using auth token file or auth string
+        if (envSTokenPath==null) throw new Error(`CONFIG.AUTHTOKENFILE ${(envSTokenPath==null? 'er ikke satt. Gjøres i config eller med environment variabel f.eks export AUTHTOKENFILE="/home/m12345/.stinkytoken.key". ' : `"${envSTokenPath}" finnes ikke.`)}`)
+        if (CONFIG.AUTHTOKENSTRING==null && !getcat.auth.isValidAccessToken(envSTokenPath, 24)) throw new Error(`CONFIG.AUTHTOKENFILE hentet fra "${envSTokenPath}" har gått ut og må fornyes.`)
 
         return getcat.auth.getAccessToken(envSTokenPath, tokenForSystem, tokenForIdent).then( (authToken) => {
           return getcat.requests._inner.requestPostAndWaitForResponseWithToken(method, authToken, systemUrl, postJsonObject, klientId, acceptType, contentType)
         })
       },
-      requestPostAndWaitForResponseWithToken: async function(method, authTokenString, systemUrl, postJsonObject, klientId, acceptType='application/json', contentType='application/json') {
+      requestPostAndWaitForResponseWithToken: async function(method, authTokenString, systemUrl, postJsonObject, klientId='getcat-'+VERSION, acceptType='application/json', contentType='application/json') {
         let options = {
           'method': method,
           'url': systemUrl,
           'headers': {
             'Klientid': klientId,
-            'Korrelasjonsid': getcat.misc.uuid(), // Example: 'f49fb193-4cb6-4bb8-a3da-71797eb7d6da',
-            'Meldingsid': getcat.misc.uuid(), // Example: 'e7f28c16-bb3b-4125-9886-d8b1de9f5201',
+            'Korrelasjonsid': getcat.misc.uuid(), // Eksempel: 'f49fb193-4cb6-4bb8-a3da-71797eb7d6da',
+            'Meldingsid': getcat.misc.uuid(), // Eksempel: 'e7f28c16-bb3b-4125-9886-d8b1de9f5201',
             'Content-Type': contentType,
             'Accept': acceptType,
             'Authorization': authTokenString
@@ -148,12 +241,17 @@ const getcat = {
         if(method=='GET' && postJsonObject=='') {
           options = getcat.misc.removeProperty(options, 'body')
         }
+        // If no auth header, remove it
+        if(authTokenString===false) {
+          options = getcat.misc.removeProperty(options, 'Authorization')
+        }
+
         // If option set to null, remove it
         if (options.headers['Content-Type']==null) {
           options.headers = getcat.misc.removeProperty(options.headers, 'Content-Type')
         }
 
-        // Add extra required headers 
+        // Legg på ekstra headers påkrevet
         defaultHeaders.forEach( ({ key, value }) => {
           options.headers[key] = value
         })
@@ -183,12 +281,12 @@ const getcat = {
         // console.log(`----DEBUG: ${fetchUrl} | ${basicAuthBase64string} `)
         try {
           const response = await fetch(fetchUrl, { method: 'GET', headers: { 'Authorization' : 'Basic '+basicAuthBase64string, 'Accept' : '*/*', 'X-CSRF' : 1 } })
-          if (response.status != 200) { reject('Invalid status code:' + response.status) }
-          return response.json()
+          if (response.status != 200) { throw new Error('Non-successful http status code:' + response.status) }
+          return response.text()
         }
         catch(resError) {
           console.error('Authorize request failed: ', (resError.response.error? resError.response.error : resError))
-          checkForCertificateError(resError.cause.code)
+          checkForCertificateError((resError.cause?resError.cause.code : resError))
           throw resError
         }
       },
@@ -196,11 +294,11 @@ const getcat = {
         // console.log(`----DEBUG: ${fetchUrl} | ${token} | ${bodyString} `)
         try {
           const response = await fetch(fetchUrl, { method: "POST", body: bodyString, headers: { 'Authorization' : 'Bearer '+token, 'Content-Type' : 'application/x-www-form-urlencoded' } })
-          if (response.status != 200) { throw new Error('Invalid status code:' + response.status) }
+          if (response.status > 299) { throw new Error('Non-successful http status code:' + response.status) }
           return response.json()
         }
         catch(resError) {
-          checkForCertificateError(resError.cause.code)
+          checkForCertificateError((resError.cause?resError.cause.code : resError))
           throw resError
         }
       }
@@ -213,6 +311,12 @@ const getcat = {
     },
     setUserName: function(userNameForToken=null) {
       tokenForIdent = userNameForToken
+    },
+    setBasicAuth: function(base64hashString) {
+      tokenBase64 = base64hashString
+    },
+    setBasicAuthLogin: function(username, pass) {
+      getcat.auth.setBasicAuth(getcat.misc.base64Encode(username+':'+pass))
     },
     isValidAccessToken: function(tokenFilePath, validPeriodeHours=24) {
       if (fs.existsSync(tokenFilePath)===false) return false
@@ -267,6 +371,12 @@ const getcat = {
     removeProperty : function(targetObject, property) {
       const { [property]: unused, ...rest } = targetObject
       return rest
+    },
+    base64Encode : function(str) {
+      return Buffer.from(str).toString('base64')
+    },
+    base64Decode : function(strCoded) {
+      return Buffer.from(strCoded).toString('utf8')
     }
   },
   /* FILE */
@@ -319,5 +429,7 @@ const getcat = {
   }
 }
 
+// Export to outside script usage
 module.exports = getcat
-
+// Enable command line argument handling
+processCommandlineArguments(process.argv)
